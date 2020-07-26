6c7057c4e423f98fc24ee9b15733aa1d55225b06
mark.j0hnst0n <friendorfaux6@gmail.com>
2020-07-26 16:03:12 +0000

    all sprites drawn on game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,13 +15,43 @@ fg.src = "assets/images/fg.png";
 pipeUp.src = "assets/images/pipeUp.png";
 pipeDown.src = "assets/images/pipeDown.png";
 
+
+var gap = 80;
+var constant = pipeUp.height+gap;
+
+var bX = 10;
+var bY = 150;
+
+var gravity = 1;
+
+// key press event
+
+document.addEventListener("keydown",moveUp);
+
+function moveUp(){
+    bY -= 20;
+}
+
+// pipe coordinates
+
+var pipe = [];
+
 // draw images
 
 function draw(){
 
     ctx.drawImage(bg,0,0);
 
+    ctx.drawImage(pipeDown,150,0);
+    ctx.drawImage(pipeUp,150,0+constant);
 
+    ctx.drawImage(fg,0,cvs.height - fg.height);
+
+    ctx.drawImage(bird,bX,bY);
+
+    bY += gravity;
+
+    requestAnimationFrame(draw);
 }
 
 draw();
