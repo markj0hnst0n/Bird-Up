05531fac17b2f48bdbfbf18d1fd7010ea5479c2e
mark.j0hnst0n <friendorfaux6@gmail.com>
2020-07-28 12:27:24 +0000

    infinite loop bug fixed and collision detection defined as a variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,11 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
 
-var bird = new Image();
-var bg = new Image();
-var fg = new Image();
-var pipeNorth = new Image();
-var pipeSouth = new Image();
+let bird = new Image();
+let bg = new Image();
+let fg = new Image();
+let pipeNorth = new Image();
+let pipeSouth = new Image();
 
 // load images
 
@@ -16,13 +16,13 @@ pipeNorth.src = "assets/images/pipeDown.png";
 pipeSouth.src = "assets/images/pipeUp.png";
 
 
-var gap = 200;
-var constant = pipeNorth.height+gap;
+const gap = 200;
+const constant = pipeNorth.height+gap;
 
-var bX = 10;
-var bY = 150;
+let bX = 10;
+let bY = 150;
 
-var gravity = 1.5;
+const gravity = 1.5;
 
 // key press event
 
@@ -47,7 +47,7 @@ function draw(){
 
     ctx.drawImage(bg,0,0);
 
-    for(var i = 0; i < pipe.length; i++){
+    for(let i = 0; i < pipe.length; i++){
         ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
         ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
 
@@ -56,14 +56,16 @@ function draw(){
         if( pipe[i].x == 125 ){
             pipe.push({
                 x : cvs.width,
-                y : Math.floor(Math.random()*pipeNorth.height) -
-                pipeNorth.height
+                y : Math.floor(Math.random()*pipeNorth.height) - pipeNorth.height
             });
         }
 
         // Collision Detection
-        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >= cvs.height - fg.height){
-            location.reload();
+        
+        let collision = bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >= cvs.height - fg.height;
+
+        if(collision){
+            return;
         }
 
     }
