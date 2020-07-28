fbcd78d5f3bec3c5667ea05582d918381e24fc24
mark.j0hnst0n <friendorfaux6@gmail.com>
2020-07-28 08:47:56 +0000

    pipe images resized

--- assets/js/game.js
+++ assets/js/game.js
@@ -62,7 +62,7 @@ function draw(){
         }
 
         // Collision Detection
-        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant)){
+        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >= cvs.height - fg.height){
             location.reload();
         }
 
